21d49e9a9c3d3dfec7549227a65df7d215c12d9d
djendja <andjela.dj5@gmail.com>
2021-02-15 22:45:38 +0100

    Stranica za fajlove,dokumente itd

---
+++ fajlovi.php
@@ -0,0 +1,84 @@
+<?php
+  include 'glavnaSesija.php';
+  include 'konekcija.php';
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Informacioni sistem za osposobljavanje kandidata za vozače</title>
+
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+<meta content="" name="description">
+<meta content="" name="keywords">
+<meta content="" name="author">
+
+<link rel="shortcut icon" href="favicon.ico">
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
+<link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<link href="assets/pages/css/animate.css" rel="stylesheet">
+<link href="assets/plugins/fancybox/source/jquery.fancybox.css" rel="stylesheet">
+<link href="assets/pages/css/components.css" rel="stylesheet">
+<link href="assets/pages/css/slider.css" rel="stylesheet">
+<link href="assets/onepage/css/style.css" rel="stylesheet">
+<link href="assets/onepage/css/style-responsive.css" rel="stylesheet">
+<link href="assets/onepage/css/themes/red.css" rel="stylesheet" id="style-color">
+<link href="assets/onepage/css/custom.css" rel="stylesheet">
+</head>
+<body class="menu-always-on-top">
+
+
+  <?php include 'meni.php'; ?>
+
+    <div class="about-block content content-center" id="about">
+        <div class="container">
+            <h2><strong><b>Važni fajlovi, dokumenta,rasporedi i drugo<b></strong></h2>
+            <h4>Lista dokumenata</h4>
+            <?php
+                foreach (glob("fajlovi/*.pdf") as $filename) {
+                    $file = basename($filename, ".pdf");
+                    echo "<a href='$filename' target='blank'>$file</a><br>";
+                }
+                ?>
+
+        </div>
+    </div>
+
+
+    <?php include 'footer.php'; ?>
+
+<script src="assets/plugins/jquery.min.js" type="text/javascript"></script>
+<script src="assets/plugins/jquery-migrate.min.js" type="text/javascript"></script>
+<script src="assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="assets/plugins/fancybox/source/jquery.fancybox.pack.js" type="text/javascript"></script><!-- pop up -->
+<script src="assets/plugins/jquery.easing.js"></script>
+<script src="assets/plugins/jquery.parallax.js"></script>
+<script src="assets/plugins/jquery.scrollTo.min.js"></script>
+<script src="assets/onepage/scripts/jquery.nav.js"></script>
+<script src="assets/onepage/scripts/layout.js" type="text/javascript"></script>
+<script src="assets/pages/scripts/bs-carousel.js" type="text/javascript"></script>
+<script>
+    jQuery(document).ready(function() {
+        Layout.init();
+    });
+</script>
+<script>
+    function pretraziPoRoku(id){
+      $.ajax({
+        url: "pretraziPoRoku.php",
+        data: {id:id},
+        success: function(tabela){
+          $("#rezultat").html(tabela);
+        }
+      })
+    }
+</script>
+<script>
+pretraziPoRoku(0);
+</script>
+</body>
+</html>
